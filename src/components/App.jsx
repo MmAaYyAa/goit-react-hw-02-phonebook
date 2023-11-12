4f875abb6460b0fcdb1d649bd7a78fded08ac1ac
MmAaYyAa <132580599+MmAaYyAa@users.noreply.github.com>
2023-11-12 18:33:05 +0100

    Update App.jsx
    functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,22 @@ export class App extends Component {
   state = {
     contacts: [],
   };
+
+  handleAddContact = newContact =>
+    this.setState(({ contacts }) => ({ contacts: [...contacts, newContact] }));
+
+  handleCheckUniqueContact = name => {
+    const { contacts } = this.state;
+    const isExistContact = !!contacts.find(contact => contact.name === name);
+    isExistContact && alert('Contact is already exist');
+    return !isExistContact;
+  };
+
   render() {
     return (
       <>
         <h2>Phonebook</h2>
-        <ContactForm></ContactForm>
+        <ContactForm onAdd={this.handleAddContact}></ContactForm>
       </>
     );
   }
